d7f2a5b330492a67c05a3cf00447fa4391461553
yashmittal2 <yashmittal56789@gmail.com>
2023-05-02 13:13:37 +0530

    1

---
+++ src/components/NoteComponents/Notes.js
@@ -0,0 +1,35 @@
+import { React, useState, useEffect } from "react";
+import "../css/Note.css";
+import CreateNote from "./CreateNote";
+import Note from "./Note";
+import { v4 as uuid } from "uuid";
+
+function Notes() {
+  const [notes, setNotes] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  const textHandler = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const saveHandler = () => {
+    setNotes((prevState) => [...prevState, { id: uuid(), text: inputText }]);
+    setInputText("");
+  };
+
+  const deleNote =(id) =>{
+    const filteredNOtes = notes.filter((note)=> note.id!==id)
+    setNotes(filteredNOtes);
+  };
+   
+  
+
+  return (
+    <div className="notes">
+      <Note />
+      <Note />
+      <CreateNote />
+    </div>
+  );
+}
+export default Notes;
